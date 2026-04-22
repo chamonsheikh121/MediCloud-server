3e0e9575bcf6e069988bba39712387c8466a3479
Chamon Ali <studentchamon1212@gmail.com>
2026-04-22 20:12:28 +0600

    Remove unused imports and clean up auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -7,13 +7,4 @@ import { AuthController } from './auth.controller';
   controllers: [AuthController],
 })
 export class AuthModule {}
-/// 
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-
-@Module({
-  providers: [AuthService],
-  controllers: [AuthController],
-})
-export class AuthModule {}
+//rsprovidersv
