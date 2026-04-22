e97c69d003fe493496d666c5c06c205f8787d7ee
Chamon Ali <studentchamon1212@gmail.com>
2026-04-22 20:11:46 +0600

    Fix formatting and remove duplicate code in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -7,3 +7,13 @@ import { AuthController } from './auth.controller';
   controllers: [AuthController],
 })
 export class AuthModule {}
+/// 
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
